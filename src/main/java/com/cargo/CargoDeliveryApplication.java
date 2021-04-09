358bafcd8310d353411aa8a646285bf548b9a6f8
OoXsokTB <so_simple@ukr.net>
2021-04-09 15:39:12 +0300

    init

---
+++ src/main/java/com/cargo/CargoDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.cargo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargoDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CargoDeliveryApplication.class, args);
+	}
+
+}
