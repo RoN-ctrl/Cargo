945e7719e6ff0c5efc2997ad2fff539f168033ad
OoXsokTB <so_simple@ukr.net>
2021-04-20 17:53:33 +0300

    LoggerInterceptor implemented

--- src/main/java/com/cargo/interceptor/LoggerInterceptor.java
+++ src/main/java/com/cargo/interceptor/LoggerInterceptor.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
 
 @Slf4j
 @Component
@@ -17,7 +18,7 @@ public class LoggerInterceptor implements HandlerInterceptor {
                              Object handler) throws Exception {
 
         log.info("preHandle: [" + request + "][" + request.getMethod() + "]"
-                + request.getRequestURI());
+                + request.getRequestURI() + getParameters(request));
 
         return true;
     }
@@ -28,4 +29,42 @@ public class LoggerInterceptor implements HandlerInterceptor {
 
         log.info("postHandle: [" + request + "]");
     }
+
+    private String getParameters(HttpServletRequest request) {
+        StringBuffer posted = new StringBuffer();
+        Enumeration<?> e = request.getParameterNames();
+        if (e != null) {
+            posted.append("?");
+        }
+        while (e.hasMoreElements()) {
+            if (posted.length() > 1) {
+                posted.append("&");
+            }
+            String curr = (String) e.nextElement();
+            posted.append(curr + "=");
+            if (curr.contains("password")
+                    || curr.contains("pass")
+                    || curr.contains("pwd")) {
+                posted.append("*****");
+            } else {
+                posted.append(request.getParameter(curr));
+            }
+        }
+        String ip = request.getHeader("X-FORWARDED-FOR");
+        String ipAddr = (ip == null) ? getRemoteAddr(request) : ip;
+        if (ipAddr!=null && !ipAddr.equals("")) {
+            posted.append("&_psip=" + ipAddr);
+        }
+        return posted.toString();
+    }
+
+    private String getRemoteAddr(HttpServletRequest request) {
+        String ipFromHeader = request.getHeader("X-FORWARDED-FOR");
+        if (ipFromHeader != null && ipFromHeader.length() > 0) {
+            log.debug("ip from proxy - X-FORWARDED-FOR : " + ipFromHeader);
+            return ipFromHeader;
+        }
+        return request.getRemoteAddr();
+    }
+
 }
